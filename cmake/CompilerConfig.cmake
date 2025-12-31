74d7d640275c16b36bf1d79c042846f436a87b79
Ghevond <ghevond.98@gmail.com>
2025-12-31 11:06:52 +0400

    Add sanitizer support

--- cmake/CompilerConfig.cmake
+++ cmake/CompilerConfig.cmake
@@ -19,3 +19,25 @@ function(cortex_apply_warnings TARGET_NAME)
         -Wsign-conversion
     )
 endfunction()
+
+# Sanitizers (ASan, UBSan)
+function(cortex_apply_sanitizers TARGET_NAME)
+    if(CORTEX_USE_SANITIZERS)
+        if(EMSCRIPTEN)
+            message(STATUS "[${TARGET_NAME}] Enabling WASM Sanitizers")
+            target_compile_options(${TARGET_NAME} PUBLIC -fsanitize=address -fsanitize=undefined)
+            target_link_options(${TARGET_NAME} PUBLIC -fsanitize=address -fsanitize=undefined)
+        else()
+            message(STATUS "[${TARGET_NAME}] Enabling Native Sanitizers")
+            target_compile_options(${TARGET_NAME} PUBLIC 
+                -fsanitize=address 
+                -fsanitize=undefined 
+                -fno-omit-frame-pointer
+            )
+            target_link_options(${TARGET_NAME} PUBLIC 
+                -fsanitize=address 
+                -fsanitize=undefined
+            )
+        endif()
+    endif()
+endfunction()
