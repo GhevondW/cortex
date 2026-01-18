25a69c4dec13d9189aa065144dde669e6d51e66e
Ghevond <ghevond.98@gmail.com>
2026-01-18 13:49:54 +0400

    Generator

---
+++ tests/generator_test.cpp
@@ -0,0 +1,46 @@
+#include <cortex/generator.hpp>
+
+#include <gtest/gtest.h>
+
+#include <stdexcept>
+#include <vector>
+
+TEST(GeneratorTest, BasicSequence) {
+    auto gen = cortex::Generator<int>::Make([](auto& yield) {
+        yield.Yield(1);
+        yield.Yield(2);
+        yield.Yield(3);
+    });
+
+    std::vector<int> values;
+    while (gen.Next()) {
+        values.push_back(gen.DetachValue());
+    }
+
+    std::vector<int> expected = {1, 2, 3};
+    EXPECT_EQ(values, expected);
+    EXPECT_TRUE(gen.IsDone());
+}
+
+TEST(GeneratorTest, DetachValueThrowsWhenEmpty) {
+    auto gen = cortex::Generator<int>::Make([](auto& yield) {
+        yield.Yield(42);
+    });
+
+    EXPECT_THROW(gen.DetachValue(), std::logic_error);
+    EXPECT_TRUE(gen.Next());
+    EXPECT_EQ(gen.DetachValue(), 42);
+    EXPECT_THROW(gen.DetachValue(), std::logic_error);
+}
+
+TEST(GeneratorTest, ExceptionPropagation) {
+    auto gen = cortex::Generator<int>::Make([](auto& yield) {
+        yield.Yield(7);
+        throw std::runtime_error("boom");
+    });
+
+    EXPECT_TRUE(gen.Next());
+    EXPECT_EQ(gen.DetachValue(), 7);
+    EXPECT_THROW(gen.Next(), std::runtime_error);
+    EXPECT_TRUE(gen.IsDone());
+}
